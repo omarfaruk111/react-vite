9267c0f194b64a6de1ef19d0edb3c4d8d4ed6ebf
Omar Faruk <omarfaruk@droitlab.com>
2023-07-06 19:24:21 +0600

    first commit

---
+++ react-vite/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+// Pages Load
+import Header from './pages/Header/Header'
+import Footer from './pages/Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
