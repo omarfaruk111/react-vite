e42636d70c5415e1e375ab99a39733f13a087f84
Omar Faruk <omarfaruk@droitlab.com>
2023-07-10 18:27:24 +0600

    update

--- react-vite/src/pages/Home/Home.jsx
+++ react-vite/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from '../../components/About'
 import Skill from '../../components/Skill'
+import Slider from '../../components/Slider'
 
 const Home = () => {
   return (
     <>
-      <About/>
+      <About />
       <Skill />
+      <Slider />
     </>
   )
 }
