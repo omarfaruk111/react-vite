f0bb8c040aeb5883313833e31ac9f612d02a3058
Omar Faruk <omarfaruk@droitlab.com>
2023-08-13 19:45:02 +0600

    update

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 // Page Import
-import Home from './pages/Home/Home'
+//import Home from './pages/Home/Home'
+import Header from './components/Header/Header'
 // import Gallery from './components/Gallery/Gallery'
 const App = () => {
   return (
     <div>
       {/* <Gallery /> */}
-      <Home />
-      
+      {/* <Home /> */}
+      <Header />
+
     </div>
   )
 }
--- react-vite/src/components/Header/Header.jsx
+++ react-vite/src/components/Header/Header.jsx
@@ -1,46 +1,29 @@
-import React from 'react'
-import Logo from '../../assets/images/omar-faruk-logo.svg';
+import React, { useState } from 'react'
+import { motion } from "framer-motion"
+import { BsArrowRight } from "react-icons/bs";
+import { FaHome } from "react-icons/fa";
 const Header = () => {
+  const [open, setOpen] = useState(true);
   return (
-    <div className='py-0 sticky top-0 z-[9999] bg-black'>
-      <div className='container m-auto'>
-          
-        
-      <div className="navbar justify-between">
-        <div className="navbar-start md:justify-between md:w-full">
-          <div className="dropdown hidden">
-            <label tabIndex={0} className="btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>Skill</a></li>
-            <li><a href='#'>Service</a></li>
-            <li><a href='#'>Portfolio</a></li>
-            <li><a href='#'>Blog</a></li>
-            </ul>
-          </div>
-          <a href='#' className="btn-ghost normal-case text-xl">
-            <img className='w-[200px] h-[80px]' src={Logo} alt="" />
-          </a>
-        </div>
-        <div className="navbar-center md:hidden lg:flex">
-          <ul className="flex flex-row gap-6 text-lg uppercase px-1 text-white">
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>Skill</a></li>
-            <li><a href='#'>Service</a></li>
-            <li><a href='#'>Portfolio</a></li>
-            <li><a href='#'>Blog</a></li>
-          </ul>
-        </div>
-      </div>
+    <div className=''>
+      <motion.div className={`bg-black h-screen p-5 pt-8 ${open ? "w-72" : "w-24"} duration-300 relative `}>
 
+        <BsArrowRight className={`text-white text-4xl absolute top-7 -right-4 border border-fuchsia-600 cursor-pointer rounded-3xl bg-fuchsia-800 p-1 ${!open && "rotate-180"} `} onClick={() => setOpen(!open)} />
 
-      </div>
+        <div className={`${open && "inline-flex"} ${!open && "inline-block"}`}>
+          <FaHome className={`text-white float-left mr-2 text-4xl block cursor-pointer bg-indigo-500 duration-500 rounded ${open && "rotate-[360deg]"} `} />
+          <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>Omar Faruk</h1>
+        </div>
+
+        <div className=''>
+
+        </div>
+
+      </motion.div>
     </div>
   )
 }
 
 export default Header
+
+// https://www.youtube.com/watch?v=MszSqhEw__8
