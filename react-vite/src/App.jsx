a3194d92cea34692ea4db32f6991746a61678926
Omar Faruk <omarfaruk@droitlab.com>
2023-09-07 11:50:25 +0600

    update

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -5,9 +5,11 @@ import Home from './pages/Home/Home'
 import Header from './components/Header/Header'
 import { Example } from './components/Menu/Example'
 // import Gallery from './components/Gallery/Gallery'
+import Book from './components/Booking/Book'
 const App = () => {
   return (
     <div>
+      <Book />
       {/* <Gallery /> */}
       <Example />
       {/* <Home /> */}
